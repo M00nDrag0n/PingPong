31fa6af1cba2999117dfb63b6e02a866990e826f
Student <example@example.com>
2019-06-26 21:07:39 -0700

    save the crap

--- Js/Scripts.js
+++ Js/Scripts.js
@@ -1,17 +1,28 @@
+var pingPong = function(number) {
+  if (number % 3 === 0) {
+    return "ping";
+  }
+  else if  (number % 5 !== 0) {
+    return "pong";
+  }
+  else if (number % 15 === 0 ) {
+    return "pingpong"
+  }
+  else {
+    return number;
+  }
+}
+
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#number").val());
-    var result = pingPong(number);
+    var userNumber = parseInt($("input#number").val());
+    console.log(userNumber);
+    var result = pingPong(userNumber);
     $("#result").text(result);
-    var pingPong = function(number) {
-      if (input % 3 === 0) && (input % 5 !== 0)) || (input % 15 === 0 )) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-  });
-});
 
-STOPPING POINT: https://www.learnhowtoprogram.com/intro-to-programming-evening/arrays-looping-8a0d105a-0895-42ec-b85c-d45f7f2eba79/leap-year-part-2 
+
+
+  });
+  $("#result").show();
+});
